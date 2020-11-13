8471bf0f0c4845bf4d43dafc17c846ebf5cd7e60
Superleaf1995 <jesusantonio30122016@gmail.com>
2020-11-13 17:44:02 -0500

    new images :D

---
+++ xaiecon/assets/public/landscape/LICENSE.md
@@ -0,0 +1,5 @@
+ls1.jpeg - https://pixabay.com/photos/mountains-landscape-snow-5727541/
+ls2.jpeg - https://pixabay.com/photos/mountain-cloud-peak-sea-of-clouds-5678172/
+ls3.jpeg - https://pixabay.com/photos/yellowstone-national-park-wyoming-1581879/
+
+Licensed under Pixabay License (https://pixabay.com/service/license/)
--- xaiecon/modules/core/asset.py
+++ xaiecon/modules/core/asset.py
@@ -3,8 +3,6 @@
 #
 
 import os
-import rjsmin
-import rcssmin
 
 from werkzeug.utils import secure_filename
 from flask import Blueprint, render_template, send_file
@@ -12,28 +10,14 @@ from xaiecon.modules.core.cache import cache
 
 asset = Blueprint('asset',__name__,template_folder='templates')
 
-# optimize assets to save space and time
-def optimize_all():
-	for file in os.listdir('./xaiecon/assets/private'):
-		# minify css
-		if file.endswith('css'):
-			infile = open(os.path.join('./xaiecon/assets/private',file),'r')
-			outfile = open(os.path.join('./xaiecon/assets/public',file),'w')
-			outfile.write(rcssmin.cssmin(infile.read()))
-		# minify css
-		elif file.endswith('js'):
-			infile = open(os.path.join('./xaiecon/assets/private',file),'r')
-			outfile = open(os.path.join('./xaiecon/assets/public',file),'w')
-			outfile.write(rjsmin.jsmin(infile.read()))
-		else:
-			infile = open(os.path.join('./xaiecon/assets/private',file),'rb')
-			outfile = open(os.path.join('./xaiecon/assets/public',file),'wb')
-			outfile.write(infile.read())
-
 @asset.route('/assets/<asset_name>', methods = ['GET'])
 def send_asset(asset_name):
 	final_filename = os.path.join('./assets/public',secure_filename(asset_name))
 	return send_file(final_filename)
 
-optimize_all()
+@asset.route('/assets/landscape/<asset_name>', methods = ['GET'])
+def send_asset_landscape(asset_name):
+	final_filename = os.path.join('./assets/public/landscape',secure_filename(asset_name))
+	return send_file(final_filename)
+
 print('Asset serving ... ok')

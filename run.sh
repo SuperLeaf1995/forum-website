2c59cd8b14bc83c46f994a1063aa38248cddc1f8
Superleaf1995 <jesusantonio30122016@gmail.com>
2020-11-15 15:24:20 -0500

    style fixes for fadein buttons

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
-# Flask config
 export FLASK_APP=xaiecon
 export FLASK_ENV=development
-export SERVER_CONFIG_FILE=flask_config.cfg
-
-# UTF8 stuff
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
-# Other envars
 export SQLALCHEMY_URL=postgresql://xaiuser:xaipass@localhost/xaiecon
-export HCAPTCHA_SITE_KEY='sitekey'
-export HCAPTCHA_SECRET_KEY='secretkey'
 
 flask run --host=0.0.0.0

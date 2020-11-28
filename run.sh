482ec7c2ab2947669e729edf3612fd346004f6aa
Superleaf1995 <jesusantonio30122016@gmail.com>
2020-11-28 00:47:13 -0500

    activity pub

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 export FLASK_APP=xaiecon
 export FLASK_ENV=development
 export SQLALCHEMY_URL=postgresql://xaiuser:xaipass@localhost/xaiecon
+export DOMAIN_NAME=localhost:5000
 
 flask run --host=0.0.0.0
--- xaiecon/modules/core/fediverse.py
+++ xaiecon/modules/core/fediverse.py
@@ -12,6 +12,8 @@
 
 # TODO: implement activitypub
 
+import os
+
 from flask import Blueprint, render_template, request, jsonify
 from flask_babel import gettext
 
@@ -23,8 +25,28 @@ from xaiecon.modules.core.wrappers import login_required
 
 fediverse = Blueprint('fediverse',__name__,template_folder='templates/fediverse')
 
+# Inbox for user
+@fediverse.route('/acpub/user/<user_id>/inbox', methods = ['POST'])
+def inbox(user_id=0):
+	data = request.json
+	
+	if data['@context'] != 'https://www.w3.org/ns/activitystreams':
+		return '',400
+	
+	atype = data.get('type')
+	
+	# Upvote
+	if atype == 'Like':
+		actor = data.get('actor')
+		object = data.get('object')
+		published = data.get('published')
+		
+		
+	
+	return '',201
+
 # Return user object
-@fediverse.route('/acpub/user/<user_id>/object')
+@fediverse.route('/acpub/user/<user_id>/object', methods = ['GET'])
 def user_object(user_id=0):
 	db = open_db()
 	user = db.query(User).filter_by(id=user_id).first()
@@ -34,31 +56,19 @@ def user_object(user_id=0):
 	data = {
 		'@context':'https://www.w3.org/ns/activitystreams',
 		'type':'Person',
-		'id':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/object',
-		'following':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/follwing',
-		'followers':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/follwers',
-		'liked':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/liked',
-		'inbox':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/inbox',
-		'outbox':'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/outbox',
-		'preferredUsername':'{user.username}',
-		'name':'{user.name}',
-		'summary':'{user.descr}',
-		'icon':['https://{os.environ.get("DOMAIN_NAME")}/user/thumb?uid={user_id}']
+		'id':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/object',
+		'following':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/follwing',
+		'followers':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/follwers',
+		'liked':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/liked',
+		'inbox':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/inbox',
+		'outbox':f'https://{os.environ.get("DOMAIN_NAME")}/acpub/user/{user_id}/outbox',
+		'preferredUsername':f'{user.username}',
+		'name':f'{user.name}',
+		'summary':f'{user.biography}',
+		'icon':[f'https://{os.environ.get("DOMAIN_NAME")}/user/thumb?uid={user_id}']
 	}
 	
 	return jsonify(data),200
-
-@fediverse.route('/fediverse/receive')
-def receive(u=None):
-	data = request.json
-	
-	# Verify that data is ok
-	if data['@context'] != 'https://www.w3.org/ns/activitystreams':
-		return '',400
-	
-	
-	
-	return '',200
 
 # After this, our server will gladly receive AP stuff from this IP
 # And that IP will also be sent some of our info.

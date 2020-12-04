4ec5cace9e1983651d581eb932f85b774f1b092f
Superleaf1995 <jesusantonio30122016@gmail.com>
2020-12-04 15:01:46 -0500

    made the tests better

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import requests
-import time
-import sys
 import os
 import unittest
+import threading
 
 from xaiecon.classes.base import open_db
 from xaiecon.__init__ import app
@@ -14,7 +13,23 @@ class TestFlaskApp(unittest.TestCase):
 		db = open_db()
 		db.close()
 	
+	def test_app(self):
+		x = threading.Thread(target=app_start)
+		x.daemon = True
+		x.start()
+		
+		headers = {'User-Agent':'webapp-tester'}
+		resp = requests.get('http://localhost:5000/',headers=headers)
+		print(resp)
+	
 	# TODO: Add even more tests
+
+def app_start():
+	os.environ['FLASK_APP'] = 'xaiecon'
+	os.environ['FLASK_ENV'] = 'developement'
+	os.environ['DOMAIN_NAME'] = 'localhost:5000'
+	app.run()
 
 if __name__ == '__main__':
 	unittest.main()
+	sys.exit()
